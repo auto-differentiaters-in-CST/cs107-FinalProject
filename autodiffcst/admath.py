1ab8e4b55244ebce16c411f879dd45a1c74a275f
maxli0728 <54636428+maxli0728@users.noreply.github.com>
2020-12-10 16:44:57 +0800

    to switch branch

--- autodiffcst/admath.py
+++ autodiffcst/admath.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
-import sympy as sp
+#import sympy as sp
 #import AD as AD
 #import AD_vec as VAD
 import autodiffcst.AD as AD
