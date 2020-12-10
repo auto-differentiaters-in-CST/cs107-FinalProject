fix bugs in pow,but found bugs in precision

autodiffcst/AD.py
@@ -490,22 +490,27 @@ class AD():
         
         except RecursionError:
             if isinstance(other, int) or isinstance(other, float):
-                new_val = other ** self.val
-                new_der = np.log(other) * (new_val) * self.der
-                # may need to change
-                new_der2 = (np.log(other) ** 2) * (new_val) * self.der
-                
-                if self.higher is None:
-                    return AD(val = new_val, tag = self.tag, der = new_der, der2 = new_der2, size = self.size)
+                # new_val = other ** self.val
+                # new_der = np.log(other) * (new_val) * self.der
+                # # may need to change
+                # new_der2 = (np.log(other) ** 2) * (new_val) * self.der2
+                return exp(log(other) * self)
+                # y = exp(log(other) * self)
+                # new_val = other ** self.val
+                # new_der = y.der
+                # new_der2 = y.der2
 
-                else:
-                    higher_der = np.array([1.0]*len(self.higher))
-                    for i in range(len(self.higher)):
-                        n = i + 1
-                        higher_der[i] = new_val * np.log(other) ** n
+                # if self.higher is None:
+                #     return AD(val = new_val, tag = self.tag, der = new_der, der2 = new_der2, size = self.size)
+
+                # else:
+                #     higher_der = np.array([1.0]*len(self.higher))
+                #     for i in range(len(self.higher)):
+                #         n = i + 1
+                #         higher_der[i] = new_val * np.log(other) ** n
                
-                    # return chain_rule(ad, new_val, der, der2, higher_der)
-                    return AD(val = new_val, tag = self.tag, der = new_der, der2 = new_der2, order=len(higher_der), size = self.size, higher=higher_der)
+                #     # return chain_rule(ad, new_val, der, der2, higher_der)
+                #     return AD(val = new_val, tag = self.tag, der = new_der, der2 = new_der2, order=len(higher_der), size = self.size, higher=higher_der)
 
                 # new_self = AD(val = new_val, tag = self.tag, der = new_der, der2 = new_der2, size = self.size)
        

autodiffcst/admath.py
@@ -190,7 +190,8 @@ def log(ad):  # consider different base?
             for i in range(2, len(ad.higher)):
                 n = i + 1
                 coef = fact_ad(-1, n - 1)
-                mainval = math.pow(ad.val[0], -n)
+                mainval = np.power(ad.val[0], float(-n))
+                # mainval = math.pow(ad.val[0], -n)
                 higher_der[i] = coef * mainval
             return chain_rule(ad, new_val, der, der2, higher_der)
     elif isinstance(ad, VAD.VAD):

tests/AD_test.py
@@ -154,26 +154,45 @@ def test_mul_VAD():
     assert np.sum(g.der == np.array([[4, 0], [0, 4]])) == 4, "Error: mul der for VAD is wrong."
     assert np.sum(g.der2 == np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])) == 8, "Error: mul der2 for VAD is wrong."
 
-# def test_div_AD():
-#     [x, y] = vad.VAD([3,1])
-#     f = x * y
-#     g = 4 * x
-#     assert f.val == 4, "Error: mul value for AD is wrong."
-#     assert np.sum(f.der == np.array([1, 3])) == 2, "Error: mul der for AD is wrong."
-#     assert np.sum(f.der2 == np.array([[0, 0],[0, 0]])) == 4, "Error: mul der for AD is wrong."
-#
-#     assert g.val == 12, "Error: mul value for AD is wrong."
-#     assert np.sum(f.der == np.array([4, 0])) == 2, "Error: mul der for AD is wrong."
-#     assert np.sum(f.der2 == np.array([[0, 0], [0, 0]])) == 4, "Error: mul der for AD is wrong."
-#
-#
-# def test_div_VAD():
-#     f = vad.VAD([3,1])
-#     g = 4*f
-#     assert np.sum(g.val == np.array([12, 4])) == 2, "Error: mul value for VAD is wrong."
-#     assert np.sum(g.der == np.array([[4, 0], [0, 4]])) == 4, "Error: mul der for VAD is wrong."
-#     assert np.sum(g.der2 == np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])) == 8, "Error: mul der2 for VAD is wrong."
+def test_div_AD():
+    [x, y] = vad.VAD([3,1])
+    f = x / y
+    g = 9 / x
+    assert f.val == 3, "Error: div value for AD is wrong."
+    assert np.sum(f.der == np.array([1, -3])) == 2, "Error: div der for AD is wrong."
+    assert np.sum(f.der2 == np.array([[0, -1],[-1, 6]])) == 4, "Error: div der for AD is wrong."
 
+    assert g.val == 3, "Error: div value for AD is wrong."
+    assert np.sum(g.der == np.array([-1, 0])) == 2, "Error: div der for AD is wrong."
+    assert np.sum(g.der2 == np.array([[18/27, 0], [0, 0]])) == 4, "Error: div der for AD is wrong."
+
+def test_div_VAD():
+    f = vad.VAD([3,1])
+    h = f / 3
+    g = 1 / f
+    assert np.sum(h.val == np.array([1, 1/3])) == 2, "Error: div value for AD is wrong."
+    assert np.sum(h.der == np.array([[1/3, 0], [0, 1/3]])) == 4, "Error: div der for AD is wrong."
+    assert np.sum(h.der2 == np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]]])) == 8, "Error: div der for AD is wrong."
+    print(g)
+    assert np.sum(g.val == np.array([1/3, 1])) == 2, "Error: div value for AD is wrong."
+    assert np.sum(g.der == np.array([[-1/9, 0], [0, -1]])) == 4, "Error: div der for AD is wrong."
+    assert np.sum(g.der2 == np.array([[[2/27, 0], [0, 0]], [[0, 0], [0, 2]]])) == 8, "Error: div der for AD is wrong."
+
+def test_pow_AD():
+    [x, y] = vad.VAD([3,1])
+    f = x ** 2
+    assert f.val == 9, "Error: pow value for AD is wrong."
+    assert np.sum(f.der == np.array([6, 0])) == 2, "Error: pow der for AD is wrong."
+    assert np.sum(f.der2 == np.array([[2, 0],[0, 0]])) == 4, "Error: pow der for AD is wrong."
+    g = 2 ** x
+    print(g)
+    # precision is different from np
+    assert np.sum(g.val == np.array([8.0])) == 1, "Error: pow value for AD is wrong."
+    assert np.sum(g.der == np.array([8*np.log(2), 0.0])) == 2, "Error: pow der for AD is wrong."
+    print(np.array([8*np.log(2), 0]))
+
+    assert np.sum(g.der2 == np.array([[8*np.log(2)*np.log(2), 0.0], [0., 0.]])) == 8, "Error: pow der for AD is wrong."
+    print(np.array([[8*np.log(2)*np.log(2), 0], [0, 0]]))
 
 
 