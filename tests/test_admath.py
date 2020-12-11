aa3cf0f8abb06952ab6502f4cb97b216046c1f9d
XiaohanYa <you@example.com>
2020-12-12 04:27:51 +0800

    update file path for test

--- tests/test_admath.py
+++ tests/test_admath.py
@@ -1,7 +1,7 @@
 # Use a simple (but explicit) path modification to resolve the package properly
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 import pytest
 import math
