c410e5ef305851bfe0e6e903e2c376870a4a472f
XiaohanYa <you@example.com>
2020-12-11 22:44:33 +0800

    change structure for pypi

--- src/autodiffcst/__init__.py
+++ src/autodiffcst/__init__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+import sys
+import os
 from pkg_resources import get_distribution, DistributionNotFound
-# from AD_vec import *
-# from AD import *
-# from admath import *
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from autodiffcst.AD_vec import *
+from autodiffcst.AD import *
+from autodiffcst.admath import *
 
 try:
     # Change here if project is renamed and does not equal the package name
