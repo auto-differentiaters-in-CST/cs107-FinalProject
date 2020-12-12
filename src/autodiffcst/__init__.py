5779c9fd4adc587939f687fc64863b7c9f112ccb
XiaohanYa <you@example.com>
2020-12-12 14:59:18 +0800

    change init for import

--- src/autodiffcst/__init__.py
+++ src/autodiffcst/__init__.py
@@ -4,8 +4,8 @@ import os
 from pkg_resources import get_distribution, DistributionNotFound
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from autodiffcst.AD_vec import *
-from autodiffcst.AD import *
-from autodiffcst.admath import *
+# from autodiffcst.AD import *
+# from autodiffcst.admath import *
 
 try:
     # Change here if project is renamed and does not equal the package name
